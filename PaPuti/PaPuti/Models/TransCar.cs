2b1352e74de30d1c6eb1f4d0d0046374f296d54f
Tiran Araqelyan <tirans3777@gmail.com>
2019-08-29 11:32:01 +0400

    add model for keep all data

---
+++ PaPuti/PaPuti/Models/TransCar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaPuti.Models
+{
+    public class TransCar
+    {
+        public string TownFrom { get; set; }
+        public string Townto { get; set; }
+
+        public string Model { get; set; }
+        public int Place { get; set; }
+
+        public string CarOwnerPhone { get; set; }
+        public string CarOwnerName { get; set; }
+        public string CarOwnerEmail { get; set; }
+
+        public DateTime? From { get; set; }
+        public DateTime? Clock { get; set; }
+
+    }
+}
